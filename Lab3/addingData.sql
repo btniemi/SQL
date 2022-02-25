a1df2bab521d50b183783849320f284b9dfaae29
btniemi <braedon.niemi@rocky.edu>
2022-02-25 09:11:51 -0700

    start to lab 3 stuff
    did data and tables for new project from classroom and will answer questions and finish up project soon

---
+++ Lab3/addingData.sql
@@ -0,0 +1,50 @@
+INSERT INTO users(name, hourly_rate)
+	VALUES('Colton', 59.95)
+	
+INSERT INTO users(name, hourly_rate)
+	VALUES('Jerry', 59.95)
+	
+INSERT INTO users(name, hourly_rate)
+	VALUES('Andrew', 25.50)
+	
+INSERT INTO users(name, hourly_rate)
+	VALUES('Tommy', 10.00)
+	
+INSERT INTO users(name, hourly_rate)
+	VALUES('Johnny', 125.50)
+	
+INSERT INTO bugs(title, description)
+	VALUES('Bug 1', 'A')
+	
+INSERT INTO bugs(title, description)
+	VALUES('Bug 2', 'B')
+	
+INSERT INTO bugs(title, description)
+	VALUES('Bug 3', 'C')
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(1, 1, 1.5)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(2, 1, .5)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(5, 1, .25)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(4, 2, 2)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(3, 2, .25)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(5, 2, .25)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(1, 3, 1.25)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(2, 3, 1.25)
+	
+INSERT INTO hourLog(user_id, bug_id, hours)
+	VALUES(5, 3, 1)
