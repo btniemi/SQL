90acf225f76b90a6c97a1816ee4c326f8a168a2d
btniemi <braedon.niemi@rocky.edu>
2022-03-02 16:14:43 -0700

    work on assignment
    help from austin to understand how to work on assignment because was really stuck on what was going on there

--- Lab4/part2/tables.sql
+++ Lab4/part2/tables.sql
@@ -17,6 +17,12 @@ create table Books (
 	isbn VARCHAR(50),
 	price VARCHAR(50),
 	currancy VARCHAR(50),
-	cover_image VARCHAR(50)
+	cover_image VARCHAR(50),
+	FOREIGN KEY (author_id) REFERENCES Authors(id)
 );
 
+/* you cant use alter table here so how can we migrate the data to add a foreign key without having to resupply the dropped table with all the data again??? 
+ * seems like that is a bad chioce to do in production to just drop a full table then recreate that table and add back in all the data */
+
+
+SELECT genre, id FROM Books WHERE genre = '(no genres listed)' ;
