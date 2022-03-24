a515da885da189ba35c6a20b5c78aa9f36dd9177
btniemi <braedon.niemi@rocky.edu>
2022-03-24 11:13:01 -0600

    updates
    finished assignment

---
+++ InClassWork/example-app/database/migrations/2022_03_24_165547_create_watched_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watched', function (Blueprint $table) {
+            $table->id('peopleId')->references('id')->on('people');
+            $table->id('movieId')->references('id')->on('movies');
+            $table->integer('stars');
+            $table->text('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watched');
+    }
+};
