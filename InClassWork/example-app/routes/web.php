185aea2b26fae1191085fed64afc49b20a33bb77
btniemi <70549370+btniemi@users.noreply.github.com>
2022-04-01 09:00:23 -0600

    stuff about routes in class

--- InClassWork/example-app/routes/web.php
+++ InClassWork/example-app/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    #\App\Models\User::orderby('name')->all();
+    \App\Models\User::wherenotNull('email_verified_at')->orderby('name')->get();
+
+    return view('welcome', ['users' => $users]);
 });
+
+Route::get('/test', [\App\Http\Controllers\UserController::class, 'index']);
